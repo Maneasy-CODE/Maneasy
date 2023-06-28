9b22fdefc45d81a5af728d895c1a0d15c844ec4b
Priscila Laurentino <priscila_lau@yahoo.com.br>
2023-06-28 18:48:54 -0300

    JS e scroll

---
+++ JavaScript/javascript.js
@@ -0,0 +1,8 @@
+let input_chamado = document.getElementById("input_chamado");
+
+// function fundo_amarelo(){
+if (window.getComputedStyle(input_chamado).backgroundColor == "rgb(255, 255, 255)") {
+    // input_projeto.setAttribute( "style", "background-color", "rgb(255 209 0)")
+    input_chamado.style.backgroundColor = "var(--amarelo)"
+}
+// }    
